905f5935c96ceaa8fc9ae204732851650f67a7cd
ap4gh <pandey.amrit@outlook.com>
2019-03-22 00:16:21 +0530

    bot events

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 
 const config = require('./config.json');
 const client = new Discord.Client();
 
+// read existing commands from commands directory
 client.on('message', (message) => {
   // If message does not start with command prefix, ignore them.
   if (!message.content.startsWith(config.commandPrefix) || message.author.bot)
@@ -21,6 +23,18 @@ client.on('message', (message) => {
     console.error(err);
     message.channel.send(`Command named ${commandName} does not exist!`);
   }
+});
+
+// read existing events from the events directory
+fs.readdir('./events/', (err, eventFiles) => {
+  if (err) return console.error(err);
+
+  eventFiles.forEach((file) => {
+    if (!file.endsWith === '.js') return;
+    const eventName = file.split('.')[0];
+    const event = require(`./events/${eventName}`);
+    client.on(eventName, (obj) => event.run(client, obj));
+  });
 });
 
 client.on('ready', () => {
