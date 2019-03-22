ed48eea754fb96c3e80175e5cb9d81c735a8e96e
ap4gh <pandey.amrit@outlook.com>
2019-03-22 15:14:33 +0530

    help text will now be given if command is used without any arg or --help flag.

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+const path = require('path');
 
 const config = require('./config.json');
 const client = new Discord.Client();
 
-// read existing commands from commands directory
+// run existing commands from commands directory on each message event
 client.on('message', (message) => {
   // If message does not start with command prefix, ignore them.
   if (!message.content.startsWith(config.commandPrefix) || message.author.bot)
@@ -17,11 +18,22 @@ client.on('message', (message) => {
   // command name
   const commandName = args.shift().toLowerCase();
   // check if file with commandName exist
-  try {
-    require(`./commands/${commandName}`).run(client, message, args);
-  } catch (err) {
-    console.error(err);
-    message.channel.send(`Command named ${commandName} does not exist!`);
+  if (!args || args.length < 1 || args[0].toLowerCase() === '--help') {
+    fs.readFile(
+      path.join(__dirname, 'commands/manuals', `${commandName}.txt`),
+      {encoding: 'utf8'},
+      (err, data) => {
+        if (err) throw err;
+        return message.channel.send(data);
+      }
+    );
+  } else {
+    try {
+      require(`./commands/${commandName}`).run(client, message, args);
+    } catch (err) {
+      console.error(err);
+      message.channel.send(`Command named ${commandName} does not exist!`);
+    }
   }
 });
 
