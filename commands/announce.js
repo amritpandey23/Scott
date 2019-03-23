983858e0a830ccf6f55e99d19e93a89c7b6e4ecf
ap4gh <pandey.amrit@outlook.com>
2019-03-23 17:03:00 +0530

    announce command update - enhanced now; announce manual update

--- commands/announce.js
+++ commands/announce.js
@@ -1,4 +1,47 @@
+const Discord = require('discord.js');
+const richEmbed = Discord.RichEmbed;
+
+const auth = require('../modules/auth');
+// message categories, emoji symbol and reactions
+const messageTypes = {
+  danger: [0xc93a2d, '⛔️', '🙁'],
+  warning: [0xc4ac13, '⚠️', '✅'],
+  regular: [0x2e69c9, '📣', '👍'],
+  success: [0x2ec962, '🎉', '👏']
+};
+
 exports.run = (client, message, args) => {
+  // announcement category passed by the user
+  const messageTypeUsed = args.shift().toLowerCase();
+  // message content
   const announcementMsg = args.join(' ');
-  message.channel.send(announcementMsg);
+  message.delete();
+  // if no content was passed, just throw an error
+  if (!announcementMsg || announcementMsg.length < 0)
+    return message.channel.send(
+      '```Error: cannot send an empty announcement```'
+    );
+  // invalid announcement category
+  if (!messageTypes[messageTypeUsed])
+    return message.channel.send(
+      '```' +
+        `Error: unrecognized announcement type '${messageTypeUsed}'.\nFor help run '${auth.getCommandPrefix()}help announce' ` +
+        '```'
+    );
+  // message formatting with discord rich embed
+  const embed = new richEmbed();
+  embed
+    .setTitle(`${messageTypes[messageTypeUsed][1]} Announcement`)
+    .setDescription(announcementMsg)
+    .setColor(messageTypes[messageTypeUsed][0])
+    .setTimestamp()
+    .setFooter(
+      `This announcement is created by ${message.author.username}`,
+      message.author.avatarURL
+    );
+  // send message and add reaction
+  message.channel
+    .send({embed})
+    .then((msg) => msg.react(messageTypes[messageTypeUsed][2]))
+    .catch((err) => console.error(err));
 };
