4fe1e9d4206c8b3103070314650008b4d068d3e6
ap4gh <pandey.amrit@outlook.com>
2019-03-25 15:43:42 +0530

    CommandHandler class that contains all the methods for any command

--- modules/CommandHandler.js
+++ modules/CommandHandler.js
@@ -1,39 +1,59 @@
-const Discord = require('.discord.js');
-const permission = new Discord.Permission();
-
 const commandList = require('../configurations/commands');
+const auth = require('./auth');
+const fs = require('fs');
+const path = require('path');
 
 class CommandHandler {
-  constructor(msg, client) {
+  constructor(client, msg) {
     this.msg = msg;
     this.client = client;
     this.args = msg.content
       .slice(auth.getCommandPrefix().length)
       .trim()
       .split(/ +/g);
-    this.name = args.shift().toLowerCase();
-    this.author = msg.author;
-  }
-  validateCommand() {
-    return commandList.su[this.name] || commandList.gen[this.name];
+    this.name = this.args.shift().toLowerCase();
+    this.author = msg.guild.members.find((mem) => mem.id === msg.author.id);
   }
   isEnabled() {
-    const command = this.validateCommand();
+    const command = this.validate();
     return command && command.isEnabled;
   }
   isAuthorSu() {
     return (
-      this.author.hasPermission(permission.ADMINISTRATOR) ||
-      this.author.hasPermission(permission.BAN_MEMBERS)
+      this.author.hasPermission('ADMINISTRATOR') ||
+      this.author.hasPermission('BAN_MEMBERS')
     );
   }
-  runCommand() {
-    const commandToRun = this.validateCommand();
+  sendManual() {
+    fs.readdir(path.join(__dirname, '../commands/manuals'), (err, manuals) => {
+      if (err) return console.error(err);
+      if (manuals.indexOf(`${this.name}.txt`) !== -1) {
+        fs.readFile(
+          path.join(__dirname, '../commands/manuals', `${this.name}.txt`),
+          { encoding: 'utf8' },
+          (err, data) => {
+            if (err) return console.error(err);
+            return this.author.send(data);
+          }
+        );
+      }
+    });
+    return '';
+  }
+  validate() {
+    return commandList.su[this.name] || commandList.gen[this.name];
+  }
+  run() {
+    const commandToRun = this.validate();
     if (!commandToRun) return;
     if (!this.isAuthorSu() && commandList.su[this.name])
       return this.msg.channel.send(
         'You are not authorized to run this command.'
       );
+    if (!this.args || this.args.length < 1) {
+      this.msg.channel.send(`No argument passed!`);
+      return this.sendManual();
+    }
     return require(`../commands/${this.name}.js`).run(
       this.client,
       this.msg,
@@ -41,3 +61,5 @@ class CommandHandler {
     );
   }
 }
+
+module.exports = CommandHandler;
