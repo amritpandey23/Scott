821034c963e9730464413ee95339b60dbfa8e7ca
ap4gh <pandey.amrit@outlook.com>
2019-04-03 17:14:13 +0530

    command handle for direct messages (DM)

---
+++ modules/DMCommandHandler.js
@@ -0,0 +1,56 @@
+const commandRegistry = require('../configurations/commands');
+const fs = require('fs');
+const path = require('path');
+
+class DMCommandHandler {
+  constructor(client, msg) {
+    this.msg = msg;
+    this.client = client;
+    this.args = this.msg.content
+      .slice(require('../configurations/config.json').client.botPrefix.length)
+      .trim()
+      .split(/ +/g);
+    this.name = this.args.shift().toLowerCase();
+    this.author = msg.author;
+  }
+  isEnabled() {
+    const command = this.validate();
+    return command && command.isEnabled;
+  }
+  sendManual() {
+    fs.readdir(path.join(__dirname, '../commands/manuals'), (err, manuals) => {
+      if (err) return console.error(err);
+      if (manuals.indexOf(`${this.name}.txt`) !== -1) {
+        fs.readFile(
+          path.join(__dirname, '../commands/manuals', `${this.name}.txt`),
+          { encoding: 'utf8' },
+          (err, data) => {
+            if (err) return console.error(err);
+            return this.author.send(data);
+          }
+        );
+      }
+    });
+    return '';
+  }
+  validate() {
+    return commandRegistry.gen[this.name];
+  }
+  run() {
+    const commandToRun = this.validate();
+    if (!commandToRun) return;
+    if (!this.args || this.args.length < 1) {
+      this.msg.reply(
+        'I cannot run empty commands. To read command manual, send `!help <command-name>`.'
+      );
+      return this.sendManual();
+    }
+    return require(`../commands/${this.name}.js`).run(
+      this.client,
+      this.msg,
+      this.args
+    );
+  }
+}
+
+module.exports = DMCommandHandler;
--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,9 @@
 const Authenticator = require('./Authenticator');
 const CommandHandler = require('./CommandHandler');
+const DMCommandHandler = require('./DMCommandHandler');
 
 module.exports = {
   Auth: new Authenticator(require('../configurations/config.json')),
-  CommandHandler
+  CommandHandler,
+  DMCommandHandler
 };
