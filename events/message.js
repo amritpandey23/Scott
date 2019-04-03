8880f140fb8a78c6c5ea47cb9773ba117c63d4d0
ap4gh <pandey.amrit@outlook.com>
2019-04-03 17:14:39 +0530

    added a new command handler for direct messages

--- events/message.js
+++ events/message.js
@@ -1,11 +1,14 @@
-const { Auth, CommandHandler } = require('../modules');
+const { Auth, CommandHandler, DMCommandHandler } = require('../modules');
 
 // run existing commands from commands directory on each message event
 exports.run = (client, message) => {
   // If message does not start with command prefix, ignore them.
   if (!message.content.startsWith(Auth.botPrefix) || message.author.bot) return;
-  if (message.channel.type === 'dm') return;
+  if (message.channel.type === 'dm') {
+    const dmCommandHandler = new DMCommandHandler(client, message);
+    return dmCommandHandler.run();
+  }
 
   const commandHandler = new CommandHandler(client, message);
-  commandHandler.run();
+  return commandHandler.run();
 };
