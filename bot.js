623df05e5701ee5139c33dd21d8ea15550de6d09
ap4gh <pandey.amrit@outlook.com>
2019-04-04 13:10:55 +0530

    code update

--- bot.js
+++ bot.js
@@ -1,27 +1,34 @@
 require('colors');
-const Discord = require('discord.js');
-const fs = require('fs');
-const client = new Discord.Client();
 
-// read existing events from the events directory
-fs.readdir('./events/', (err, eventFiles) => {
-  if (err) return process.stdout.write(err);
-  // scan files in event directory and load them here.
-  eventFiles.forEach((file) => {
-    if (!file.endsWith === '.js') return;
-    const eventName = file.split('.')[0];
-    const event = require(`./events/${eventName}`);
-    client.on(eventName, (obj) => {
-      event.run(client, obj);
-      const eventLog = `NAME: ${eventName}, ON: ${new Date().toString()}\n`;
-      fs.appendFile('./logs/event-activity.txt', eventLog, (err) => {
-        if (err) throw err;
-        process.stdout.write(
-          `EVENT: ${eventName} ON: ${new Date().toString()}\n`.gray
-        );
+const fs = require('fs');
+const { Client } = require('discord.js');
+
+const bot = new Client();
+
+const registerEvents = () => {
+  // read existing events from the events directory
+  fs.readdir('./events/', (err, eventFiles) => {
+    if (err) throw err;
+    // scan files in event directory and load them here.
+    eventFiles.forEach((file) => {
+      // if there is a file that is not js, ignore it
+      if (!file.endsWith === '.js') return;
+
+      const eventName = file.split('.')[0];
+      const event = require(`./events/${file}`);
+      bot.on(eventName, (obj) => {
+        // run event
+        event.run(bot, obj);
+        // log event
+        const eventLogMessage = `EVENT: ${eventName}, ON: ${new Date().toString()}\n`;
+        fs.appendFile('./logs/event-activity.txt', eventLogMessage, (err) => {
+          if (err) throw err;
+          process.stdout.write(eventLogMessage.gray);
+        });
       });
     });
   });
-});
+};
 
-module.exports = client;
+registerEvents();
+module.exports = bot;
