276e16e9daa018bea0c4d9ce7f437f938280f450
ap4gh <pandey.amrit@outlook.com>
2019-04-04 19:17:14 +0530

    registered new handler

--- configurations/events.js
+++ configurations/events.js
@@ -1,5 +1,5 @@
 module.exports = {
-  message: ['Bot.Message', 'Bot.CommandHandler', 'Bot.DMCommandHandler'],
+  message: ['Bot.Message', 'Bot.SharedCommandHandler'],
   disconnect: ['Bot.Disconnect'],
   guildMemberAdd: ['Bot.GuildMemberAdd'],
   ready: ['Bot.Ready']
