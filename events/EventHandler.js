8253f4cf47c4544800089458f5a11fed07552fb3
ap4gh <pandey.amrit@outlook.com>
2019-04-04 18:31:35 +0530

    event handler class

---
+++ events/EventHandler.js
@@ -0,0 +1,42 @@
+const events = require('../configurations/events');
+const fs = require('fs');
+
+class EventHandler {
+  constructor(client) {
+    this.client = client;
+    this._cache = {};
+  }
+
+  async load() {
+    for (const [eventName, eventFiles] of Object.entries(events)) {
+      this._cache[eventName] = {};
+      for (const eventFile of eventFiles) {
+        const event = require(`./${eventName}/${eventFile}.js`);
+        this._cache[eventName][eventFile] = new event(this.client);
+      }
+    }
+  }
+
+  logEvent(eventName) {
+    fs.appendFile(
+      './logs/event-activity.txt',
+      `EVENT: ${eventName}, ON: ${new Date().toString()}\n`,
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+
+  async onEvent(eventName) {
+    if (!this._cache[eventName])
+      return process.stdout.write(`No event created for ${eventName}\n`.red);
+    for (const eventFile of Object.keys(this._cache[eventName])) {
+      this.client.on(eventName, (...args) => {
+        this._cache[eventName][eventFile].handle(...args);
+        this.logEvent(eventName);
+      });
+    }
+  }
+}
+
+module.exports = EventHandler;
