c858bbcc0f11914c291c06d671f5c2f33d6a58b4
ap4gh <pandey.amrit@outlook.com>
2019-04-04 18:31:08 +0530

    old event files removed

--- events/disconnect.js
+++
@@ -1,5 +0,0 @@
-exports.run = (client, _) => {
-  process.stdout.write(
-    `Client is disconnected and is no longer trying to reconnect. Must be some network problem. \n`
-  );
-};
--- events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-const { Auth } = require('../modules');
-
-exports.run = (client, member) => {
-  let welcomeChannel = Auth.findChannelByName('welcome', member.guild);
-  welcomeChannel
-    ? welcomeChannel.send(`Welcome to the server ${member}`)
-    : () => {};
-};
