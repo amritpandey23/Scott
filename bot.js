3c91bcbd3344a265d165524999023475eb327013
ap4gh <pandey.amrit@outlook.com>
2019-04-04 18:32:03 +0530

    event handler class to register bot events

--- bot.js
+++ bot.js
@@ -1,34 +1,15 @@
 require('colors');
 
-const fs = require('fs');
 const { Client } = require('discord.js');
+const events = require('./configurations/events');
+const EventHandler = require('./events/EventHandler');
 
 const bot = new Client();
 
-const registerEvents = () => {
-  // read existing events from the events directory
-  fs.readdir('./events/', (err, eventFiles) => {
-    if (err) throw err;
-    // scan files in event directory and load them here.
-    eventFiles.forEach((file) => {
-      // if there is a file that is not js, ignore it
-      if (!file.endsWith === '.js') return;
+const eventHandler = new EventHandler(bot);
+eventHandler.load();
+for (event of Object.keys(events)) {
+  eventHandler.onEvent(event);
+}
 
-      const eventName = file.split('.')[0];
-      const event = require(`./events/${file}`);
-      bot.on(eventName, (obj) => {
-        // run event
-        event.run(bot, obj);
-        // log event
-        const eventLogMessage = `EVENT: ${eventName}, ON: ${new Date().toString()}\n`;
-        fs.appendFile('./logs/event-activity.txt', eventLogMessage, (err) => {
-          if (err) throw err;
-          process.stdout.write(eventLogMessage.gray);
-        });
-      });
-    });
-  });
-};
-
-registerEvents();
 module.exports = bot;
