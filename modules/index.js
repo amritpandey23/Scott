86affb0eb8be2050e84139aee2df23e36e8398ad
ap4gh <pandey.amrit@outlook.com>
2019-04-11 16:03:37 +0530

    removed old modules

--- modules/index.js
+++ modules/index.js
@@ -1,9 +1,5 @@
 const Authenticator = require('./Authenticator');
-const CommandHandler = require('./CommandHandler');
-const DMCommandHandler = require('./DMCommandHandler');
 
 module.exports = {
-  Auth: new Authenticator(require('../configurations/config.json')),
-  CommandHandler,
-  DMCommandHandler
+  Auth: new Authenticator(require('../configurations/config.json'))
 };
