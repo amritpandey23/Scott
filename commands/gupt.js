4b605754de055c39cea72dfb8211d89bf22e94d2
ap4gh <pandey.amrit@outlook.com>
2019-04-11 16:02:46 +0530

    fixed bugs

--- commands/gupt.js
+++ commands/gupt.js
@@ -1,4 +1,5 @@
 const { User } = require('discord.js');
+const { ErrorHandler } = require('../modules/utils');
 
 exports.run = (client, message, args) => {
   // collect discord user ID
@@ -8,17 +9,24 @@ exports.run = (client, message, args) => {
   // way to check whether this ID
   // belongs to a user on discord.
   if (userID.length !== 18 || !parseInt(userID)) {
-    return message.author.send('INVALID USER ID');
+    return message.author.send('You have given me an invalid user id!');
   }
   // create a discord user
   const user = new User(client, { id: userID });
 
   const messageContent = args.join(' ');
   if (!messageContent || messageContent.length === 0) {
-    return message.author.send('I cannot send an empty message! Duh! 😕');
+    return message.author.send(
+      'Please specify what do you want to send after user id.'
+    );
   }
   // send secret/anonymous message
-  user.send(`Somebody sent you an anonymous message: *${messageContent}*`);
+  user
+    .send(`Somebody sent you an anonymous message: ||${messageContent}||`)
+    .catch((err) => {
+      process.stdout.write(`${err.message}\n${err.code}\n`.red);
+      ErrorHandler(err);
+    });
 
   // cannot perform a message.delete() in a DM channel
   if (message.channel.type !== 'dm') message.delete();
