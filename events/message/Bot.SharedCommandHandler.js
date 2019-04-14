d53e76a35e8dd96faa83c7da55dbd322ff5dbcc1
ap4gh <pandey.amrit@outlook.com>
2019-04-14 17:33:44 +0530

    refactoring

--- events/message/Bot.SharedCommandHandler.js
+++ events/message/Bot.SharedCommandHandler.js
@@ -1,107 +1,122 @@
+/**
+ * Event name: Message, Command
+ * Description: One class to handle all the command files
+ * Author: Amrit Pandey - ap4gh
+ * Dependencies: fs, path, Auth, Utils, configurations
+ * License: MIT
+ */
+
 const fs = require('fs');
 const path = require('path');
-const BaseEvent = require('../BaseEvent');
-const commandRegistry = require('../../configurations/commands');
 
+const BaseEvent = require('../BaseEvent');
+const { Auth } = require('../../modules');
+const { commands, config } = require('../../configurations');
+
+/**
+ * One class to handle commands in both guild
+ * text channel as well as DM channels. All
+ * the validation methods are applied on the
+ * command before executing it.
+ */
 class SharedCommandHandler extends BaseEvent {
+  /**
+   * handler for command event.
+   */
+  async handle(message) {
+    // ignore message if it does not start with specified bot prefix
+    if (!message.content.startsWith(Auth.botPrefix) || message.author.bot)
+      return;
+    // set all the state of the command handler
+    this.msg = message;
+    this.args = this.msg.content
+      .slice(config.client.botPrefix.length)
+      .trim()
+      .split(/ +/g);
+    this.name = this.args.shift().toLowerCase();
+    this.author = message.author;
+    this.guild = message.guild || 'DM_CHANNEL';
+    this.channelType = message.channel.type;
+
+    // execute command
+    this.run();
+  }
+
+  /**
+   * check if the command is enabled by the
+   * project owner.
+   */
   isEnabled() {
     const command = this.validate();
     return command && command.isEnabled;
   }
-
+  /**
+   * check if command user is
+   * either admin or moderator
+   * of the guild.
+   */
   isAuthorSu() {
-    const messageAuthor = this.guild.members.find(
-      (mem) => mem.id === this.author.id
-    );
+    const messageAuthor =
+      this.guild.members.find((mem) => mem.id === this.author.id);
     return (
       messageAuthor.hasPermission('ADMINISTRATOR') ||
       messageAuthor.hasPermission('MANAGE_MESSAGES')
     );
   }
 
-  sendManual() {
-    fs.readdir(path.join(__dirname, '../commands/manuals'), (err, manuals) => {
-      if (err) throw `${err}`.red;
-      if (manuals.indexOf(`${this.name}.txt`) !== -1) {
-        fs.readFile(
-          path.join(__dirname, '../commands/manuals', `${this.name}.txt`),
-          { encoding: 'utf8' },
-          (err, data) => {
-            if (err) throw `${err}`.red;
-            return this.author.send(data);
-          }
-        );
-      }
-    });
-    return '';
-  }
-
+  /**
+   * check if command is valid and
+   * is able to run in the channel
+   */
   validate() {
     return this.channelType === 'dm'
-      ? commandRegistry.gen[this.name]
-      : commandRegistry.su[this.name] || commandRegistry.gen[this.name];
+      ? commands.gen[this.name]
+      : commands.su[this.name] || commands.gen[this.name];
   }
 
-  run() {
-    const commandToRun = this.validate();
-    if (!commandToRun) return;
-    if (
-      this.channelType !== 'dm' &&
-      !this.isAuthorSu() &&
-      commandList.su[this.name]
-    )
-      return this.msg.channel.send(
-        "You are not authorized to run 'su' commands. To know more send `!help su`."
-      );
-    if (!this.args || this.args.length < 1) {
-      this.msg.reply(
-        'I cannot run empty commands. To read command manual, send `!help <command-name>`.'
-      );
-      return this.sendManual();
-    }
-    require(`../../commands/${this.name}.js`).run(
-      this.client,
-      this.msg,
-      this.args
-    );
-    return this.logUsage();
-  }
-
+  /**
+   * log command use details via this method
+   */
   logUsage() {
-    const logMessage = `COMMAND: ${this.name}, ARGUMENTS: [${this.args.join(
-      ', '
-    )}], BY: {${this.author.id}, ${this.author.username}}, GUILD: {${
-      this.guild.id
-    }, ${
-      this.guild === 'DM CHANNEL' ? this.guild : this.guild.name
-    }}, ON: ${new Date().toString()}\n`;
-
+    const logMessage = `COMMAND: ${this.name}, ARGUMENTS: [${this.args.join(', ')}]\
+      BY: {${this.author.id}, ${this.author.username}}\
+      GUILD: {${this.guild.id}, ${this.guild === 'DM_CHANNEL' ? this.guild : this.guild.name}}\
+      ON: ${new Date().toString()}\n`;
+    // add log to the log file
     fs.appendFile(
-      path.join(__dirname, '../../logs/command-activity.txt'),
-      logMessage,
-      (err) => {
+      path.join(__dirname, '../../logs/command-activity.txt'), logMessage, (err) => {
         if (err) throw err;
-        process.stdout.write(
-          `COMMAND: ${this.msg.content.magenta} ${new Date().toDateString()}\n`
-            .gray
-        );
+        process.stdout.write(`COMMAND: ${this.msg.content.magenta} ${new Date().toDateString()}\n`.gray);
       }
     );
   }
-  async handle(message) {
-    this.msg = message;
-    this.args = this.msg.content
-      .slice(
-        require('../../configurations/config.json').client.botPrefix.length
-      )
-      .trim()
-      .split(/ +/g);
-    this.name = this.args.shift().toLowerCase();
-    this.author = message.author;
-    this.guild = message.guild || 'DM CHANNEL';
-    this.channelType = message.channel.type;
 
-    this.run();
+  /**
+   * validate, import and execute command file
+   */
+  async run() {
+    // validate command
+    const commandToRun = this.validate();
+    if (!commandToRun) return;
+
+    // if channel type is 'dm' AND
+    // author is not an admin AND
+    // the command is an admin command
+    // -- end
+    if (this.channelType !== 'dm' && !this.isAuthorSu() && commandList.su[this.name])
+      return this.msg.channel.send(`You are not authorized to run 'su' commands. ` +
+        `To know more send \`${Auth.botPrefix}help su\`.`);
+
+    // if no arguments were passed -- end
+    if (!this.args || this.args.length < 1) {
+      return this.msg.reply(`Running empty commands is not currently supported. ` +
+        `To read command manual, run \`${Auth.botPrefix}help [command name]\``);
+    }
+
+    // if all the requirements and validations are met
+    // -- import and execute command file
+    require(`../../commands/${this.name}.js`).run(this.client, this.msg, this.args);
+    return this.logUsage();
   }
 }
 
