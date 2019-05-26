144f6f7b42d24c0232108bc36c6c6f928cbdbb3a
ap4gh <pandey.amrit@outlook.com>
2019-05-26 22:53:34 +0530

    slug strings utility

--- modules/utils/index.js
+++ modules/utils/index.js
@@ -2,5 +2,6 @@ module.exports = {
   ErrorHandler: require('./ErrorHandler'),
   NotifyMaintainer: require('./NotifyMaintainer'),
   checkLogFiles: require('./checkLogFiles'),
-  timeConversion: require('./timeConversion')
+  timeConversion: require('./timeConversion'),
+  slug: require('./slug')
 };
