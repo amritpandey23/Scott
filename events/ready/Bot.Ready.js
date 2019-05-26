2fb050df40b4a6bab4c01d4992c00fbefc16f34c
ap4gh <pandey.amrit@outlook.com>
2019-05-26 22:54:36 +0530

    guild member info are also saved now

--- events/ready/Bot.Ready.js
+++ events/ready/Bot.Ready.js
@@ -1,54 +1,78 @@
 const BaseEvent = require('../BaseEvent');
 const { config } = require('../../configurations');
 const { JSONCollection } = require('../../modules');
+const { slug } = require('../../modules/utils');
+const fs = require('fs')
+const path = require('path')
 
 const guildInfo = new JSONCollection('guildInfo');
 
 class Ready extends BaseEvent {
-  // handler for event
-  async handle() {
-    process.stdout.write(
-      `Logged in as ${this.client.user.tag.black.bgYellow}\n`.blue
-    );
+	// handler for event
+	async handle() {
+		process.stdout.write(
+			`Logged in as ${this.client.user.tag.black.bgYellow}\n`.blue
+		);
 
-    this.client.user
-      .setActivity(`use ${config.client['botPrefix']}help me`)
-      .then((presence) => {
-        process.stdout.write(
-          `Activity set to ${
-            presence.game ? presence.game.name.black.bgYellow : 'none'
-          }\n`.blue
-        );
-      })
-      .catch((error) => {
-        process.stdout.write(error.red);
-      });
+		this.client.user
+			.setActivity(`use ${config.client['botPrefix']}help me`)
+			.then((presence) => {
+				process.stdout.write(
+					`Activity set to ${
+						presence.game ? presence.game.name.black.bgYellow : 'none'
+						}\n`.blue
+				);
+			})
+			.catch((error) => {
+				process.stdout.write(error.red);
+			});
 
-      this.client.guilds.array().forEach(guild => {
-        const {
-            id,
-            name, 
-            createdTimestamp, 
-            iconURL, 
-            large,
-            memberCount,
-            ownerID,
-            region,
-            verified
-        } = guild;
-    
-        guildInfo.add(id, {
-            name, 
-            createdTimestamp, 
-            iconURL, 
-            large,
-            memberCount,
-            ownerID,
-            region,
-            verified
-        })
-    })
-  }
+		this.client.guilds.array().forEach(guild => {
+			let members = {}
+			const {
+				id,
+				name,
+				createdTimestamp,
+				iconURL,
+				large,
+				memberCount,
+				ownerID,
+				region,
+				verified
+			} = guild;
+
+			guildInfo.add(id, {
+				name,
+				createdTimestamp,
+				iconURL,
+				large,
+				memberCount,
+				ownerID,
+				region,
+				verified
+			})
+
+			guild.members.forEach(member => {
+				const {
+					id,
+					joinedTimestamp,
+					user,
+					permissions
+				} = member
+				members[id] = { 
+					id, 
+					joinedTimestamp, 
+					"username": user.username,
+					"avatarURL": user.avatarURL,
+					"bot": user.bot,
+					"permissions": permissions.bitfield
+				}
+			})
+			fs.writeFileSync(path.join(__dirname, `../../data/guildMembers/${slug(guild.name)}-${guild.id}.json`), JSON.stringify(members), (err) => {
+				if (err) throw err
+			})
+		})
+	}
 }
 
 module.exports = Ready;
