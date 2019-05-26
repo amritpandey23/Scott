9fdb7cde1e699eb52fa7bc2939b6c09ca7d240f5
ap4gh <pandey.amrit@outlook.com>
2019-05-26 15:18:55 +0530

    make JSON files for schemes available if not exist

--- start.js
+++ start.js
@@ -1,6 +1,23 @@
 require('colors');
+const fs = require('fs');
+const path = require('path');
 const bot = require('./bot');
-const { Auth, Utils } = require('./modules');
+const { Auth, Utils,  JSONCollection } = require('./modules');
+const { schemes } = require('./configurations');
+
+const guildInfo = new JSONCollection('guildInfo');
+
+Object.keys(schemes).forEach(schemeName => {
+    fs.readdir('./data', { encoding: 'utf8' }, (err, files) => {
+        if (!files.includes(`${schemeName}.json`)) {
+            fs.writeFileSync(path.join(__dirname, `data/${schemeName}.json`), 
+            JSON.stringify(schemes[schemeName])
+            ,(err) => {
+                if (err) throw err
+            })
+        }
+    })
+})
 
 Utils.checkLogFiles();
 bot.login(Auth.botToken);
