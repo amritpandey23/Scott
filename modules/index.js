63bfa413af4b9f971325cd8642520058657b1b05
ap4gh <pandey.amrit@outlook.com>
2019-05-26 15:17:17 +0530

    A module to handle CRUD operations on JSON files

--- modules/index.js
+++ modules/index.js
@@ -2,5 +2,6 @@ const config = require('../configurations');
 
 module.exports = {
   Auth: new (require('./Authenticator'))(config.config),
-  Utils: require('./utils')
+  Utils: require('./utils'),
+  JSONCollection: require('./JSONCollection')
 };
