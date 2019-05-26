8ae0d0cebe29fea9c5d3fa49772cdfbebb4ada1a
ap4gh <pandey.amrit@outlook.com>
2019-05-26 15:17:52 +0530

    update

--- configurations/index.js
+++ configurations/index.js
@@ -2,5 +2,6 @@ module.exports = {
   api_keys: require('./api_keys.json'),
   commands: require('./commands'),
   config: require('./config.json'),
-  events: require('./events')
+  events: require('./events'),
+  schemes: require('./schemes')
 };
