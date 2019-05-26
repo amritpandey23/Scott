99a4693d80645f81b8a510cd08b1b7890cf25430
ap4gh <pandey.amrit@outlook.com>
2019-05-26 15:17:41 +0530

    scheme for JSON data

---
+++ configurations/schemes.js
@@ -0,0 +1,25 @@
+const guildInfo = {
+    "guild_id": {
+        "name": "",
+        "createdTimestamp": "",
+        "iconURL": "",
+        "large": false,
+        "memberCount": 0,
+        "ownerID": 0,
+        "region": "",
+        "verified": false
+    }
+}
+
+const guildStats = {
+    "guild_id": {
+        "joined_last_week": 0,
+        "joined_this_week": 0,
+        "joined_last_month": 0
+    }
+}
+
+module.exports = {
+    guildInfo,
+    guildStats
+}
