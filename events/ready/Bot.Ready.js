3c0980db03bd92e9b69222d1b6661081a95f82a9
Amrit Pandey <amritpandey@tuta.io>
2019-06-08 15:42:50 +0530

    member info are not saved now

--- events/ready/Bot.Ready.js
+++ events/ready/Bot.Ready.js
@@ -1,11 +1,5 @@
 const BaseEvent = require('../BaseEvent');
 const { config } = require('../../configurations');
-const { JSONCollection } = require('../../modules');
-const { slug } = require('../../modules/utils');
-const fs = require('fs')
-const path = require('path')
-
-const guildInfo = new JSONCollection('guildInfo');
 
 class Ready extends BaseEvent {
 	// handler for event
@@ -27,51 +21,6 @@ class Ready extends BaseEvent {
 				process.stdout.write(error.red);
 			});
 
-		this.client.guilds.array().forEach(guild => {
-			let members = {}
-			const {
-				id,
-				name,
-				createdTimestamp,
-				iconURL,
-				large,
-				memberCount,
-				ownerID,
-				region,
-				verified
-			} = guild;
-
-			guildInfo.add(id, {
-				name,
-				createdTimestamp,
-				iconURL,
-				large,
-				memberCount,
-				ownerID,
-				region,
-				verified
-			})
-
-			guild.members.forEach(member => {
-				const {
-					id,
-					joinedTimestamp,
-					user,
-					permissions
-				} = member
-				members[id] = { 
-					id, 
-					joinedTimestamp, 
-					"username": user.username,
-					"avatarURL": user.avatarURL,
-					"bot": user.bot,
-					"permissions": permissions.bitfield
-				}
-			})
-			fs.writeFileSync(path.join(__dirname, `../../data/guildMembers/${slug(guild.name)}-${guild.id}.json`), JSON.stringify(members), (err) => {
-				if (err) throw err
-			})
-		})
 	}
 }
 
