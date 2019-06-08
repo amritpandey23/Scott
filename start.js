7d99f62521a5579d60433cdf7d152fccf736c290
Amrit Pandey <amritpandey@tuta.io>
2019-06-08 15:42:11 +0530

    no json files are generated with schemes now

--- start.js
+++ start.js
@@ -1,23 +1,6 @@
 require('colors');
-const fs = require('fs');
-const path = require('path');
 const bot = require('./bot');
-const { Auth, Utils,  JSONCollection } = require('./modules');
-const { schemes } = require('./configurations');
-
-const guildInfo = new JSONCollection('guildInfo');
-
-Object.keys(schemes).forEach(schemeName => {
-    fs.readdir('./data', { encoding: 'utf8' }, (err, files) => {
-        if (!files.includes(`${schemeName}.json`)) {
-            fs.writeFileSync(path.join(__dirname, `data/${schemeName}.json`), 
-            JSON.stringify(schemes[schemeName])
-            ,(err) => {
-                if (err) throw err
-            })
-        }
-    })
-})
+const { Auth, Utils } = require('./modules');
 
 Utils.checkLogFiles();
 bot.login(Auth.botToken);
