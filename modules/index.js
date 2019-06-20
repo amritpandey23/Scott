f45ed364cf4b9c3e721d2d92b1a3c2a2fdd785a3
ap4gh <amritpandey@tuta.io>
2019-06-20 13:10:25 +0530

    removed JSONCollection module

--- modules/index.js
+++ modules/index.js
@@ -2,6 +2,5 @@ const config = require('../configurations');
 
 module.exports = {
   Auth: new (require('./Authenticator'))(config.config),
-  Utils: require('./utils'),
-  JSONCollection: require('./JSONCollection')
+  Utils: require('./utils')
 };
