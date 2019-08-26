a888db8fd86ed192fd3c4617f3696b2cba3d3abb
Amrit Pandey <amrit.github@tuta.io>
2019-08-26 14:16:31 +0530

    write() now require logFile path; comments

--- utils/logger.js
+++ utils/logger.js
@@ -1,3 +1,12 @@
+/** logger.js
+ * 
+ * A utility which can be used to log
+ * colored messages in console. For any
+ * bot activity use logger instead of
+ * console.log. Anything printed using
+ * logger will also end up in log files.
+ */
+
 const
   fs = require("fs"),
   colors = require("colors/safe"),
@@ -7,18 +16,20 @@ const
 let colorScheme = settings.LOG_COLOR_SCHEME;
 colors.setTheme(colorScheme);
 
-function write(...args) {
+/** write() saves log messages in file. */
+function write(logFile=settings.PATHS.RUNTIME_LOG, ...args) {
   let line = `[${(new Date()).toISOString()}] `;      // time 
   line += args.join(' ');                             // message
   line += '\n';
 
-  return fs.appendFile(settings.PATHS.LOG_FILE, line, (err) => {
+  return fs.appendFile(logFile, line, (err) => {
     if (err) throw err;
   });
 }
 
+/** print() logs colored messages on console. */
 function print(type, ...args) {
-  write(type.toUpperCase(), args.join(' '));
+  write(logFile=settings.PATHS.RUNTIME_LOG, type.toUpperCase(), args.join(' '));
   return console.log(colors[colorScheme[type] ? type : "white"](args.join(' ')));
 }
 
