0d7c56588f49109848e1a179b32e3e7af618a236
Amrit Pandey <amrit.github@tuta.io>
2019-08-26 14:16:47 +0530

    test logger.write()

--- test/logger.js
+++ test/logger.js
@@ -1,6 +1,7 @@
 const logger = require("../utils/logger");
+const { settings } = require("../config");
 
-/** basic use */    logger.log("\n\t--BASIC USE--");
+/** basic use */    console.log("\n\t--BASIC USE--");
 logger.success("SUCCESS: Operation was successful!");           // success
 logger.info("INFO: Plain message");                             // info
 logger.log("LOG: Do not mind this message.");                   // log
@@ -8,7 +9,10 @@ logger.warn("WARN: Deprecation notice, please upgrade.");       // warn
 logger.error("ERROR: This is an error");                        // error
 logger.hl("Important information");                             // highlight
 
-/** error stack printing */     logger.log("\n\t--ERROR PRINTING AND LOGGING--");
+/** error stack printing */     console.log("\n\t--ERROR PRINTING AND LOGGING--");
 let error = new Error("something went wrong.");
 logger.error(error.message, "checkout log file for more details");      // print error message on screen.
-logger.write(error.stack);                                              // save entire trace in the log file
+logger.write(logFile=settings.PATHS.RUNTIME_LOG, error.stack);          // save entire trace in the log file
+
+/** saving logs */
+logger.write(logFile=settings.PATHS.ACTIVITY_LOG, "This is a sample I want to save in activity log file!");
