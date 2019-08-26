ce9f7111ac72c6d6af0e6a5784c94e0e58ca9133
Amrit Pandey <amrit.github@tuta.io>
2019-08-26 12:54:54 +0530

    config

---
+++ config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    settings: require("./settings")
+}
---
+++ config/settings.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+    PATHS: {
+        LOG_FILE: path.resolve(__dirname, "../logs/activity.log")
+    },
+    LOG_COLOR_SCHEME: {
+        error: ["red", "bold"],
+        warn: "yellow",
+        info: "blue",
+        log: "gray",
+        success: "green",
+        highlight: ["black", "bgYellow"]
+    }
+}
